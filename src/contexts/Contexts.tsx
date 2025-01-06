cc81bbde0a280df727542a88587d80b9422f55b2
Rene Tajos Jr. <master.isheart@gmail.com>
2025-01-06 08:39:55 +0800

    Initial commit

--- src/contexts/Contexts.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { PokemonLocalStorage } from "../types/pokemon.types";
-
-export const LocalStorageContext = createContext<PokemonLocalStorage[]>([]);
-export const ColorVariantsContext = createContext<{[color: string]: string[]}>({})
--- src/svg.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module '*.svg?react' {
-    import { FunctionComponent, SVGProps } from 'react';
-    const content: FunctionComponent<SVGProps<SVGSVGElement>>;
-    export default content;
-  }
