36d5bb2fcfd7de9b838f551932cdd8e72b07dc19
Rene Tajos Jr. <master.isheart@gmail.com>
2025-01-06 16:03:55 +0800

    Add empty state indicator in PokedexView when no pokemons are available

--- src/pages/home/views/PokedexView.tsx
+++ src/pages/home/views/PokedexView.tsx
@@ -2,6 +2,7 @@ import PokeCard from "../widgets/PokeCard";
 import { Pokemon } from "../../../types/pokemon.types";
 import clsx from "clsx";
 import { usePokedexView } from "../hooks/usePokedexView";
+import EmptyICSVG from "../../../assets/images/empty-ic.svg?react";
 
 interface PokedexGridViewProps {
     pokemons: Pokemon[]
@@ -54,6 +55,18 @@ export default function PokedexView(props: PokedexGridViewProps) {
                     ))}
 
                     <div ref={loaderRef} style={{ height: '20px' }} />
+                </div>
+            )}
+
+            {props.pokemons.length === 0 && (
+                <div
+                    className={clsx("flex", "flex-col", "absolute", "justify-center", "items-center", "top-[50%]", "translate-y-[-50%]", "left-[50%]", "translate-x-[-50%]")} 
+                >
+                    <EmptyICSVG
+                        width={100}
+                        height={100} 
+                    />
+                    <span className={clsx("text-gray-500")}>Oops! No pokemons here!</span>
                 </div>
             )}
         </>
